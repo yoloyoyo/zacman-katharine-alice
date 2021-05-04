0b252ff9e801855c5cd74b41fa399c831a9be3c1
Zack <66165382+yoloyoyo@users.noreply.github.com>
2021-05-04 18:35:48 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
+namespace SpriteKind {
+    export const PowerUp = SpriteKind.create()
+}
+function createGhost () {
+    Ghost = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . f . 1 1 1 1 f . . . . . . 
+        . . . 1 f 1 1 1 f 1 . . . . . . 
+        . . . 1 1 f 1 f 1 1 1 . . . . 1 
+        . . 1 1 1 1 1 1 1 1 1 1 . 1 1 1 
+        . . 1 1 f 1 1 1 f 1 1 1 1 1 1 . 
+        . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . 1 f 1 1 1 1 1 f 1 1 1 1 . . 
+        . . 1 1 f f f f f 1 1 1 1 1 . . 
+        . . . 1 1 1 1 3 3 1 1 1 1 1 . . 
+        . . . . . 1 1 1 1 1 1 1 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Ghost.follow(Zacman, randint(30, 60))
+    scene.cameraFollowSprite(Zacman)
+    tiles.placeOnRandomTile(Ghost, assets.tile`myTile1`)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.PowerUp, function (sprite, otherSprite) {
+    canEatGhost = true
+    otherSprite.destroy()
+    pause(5000)
+    canEatGhost = false
+})
 function createFood () {
     pizza = sprites.create(img`
         . . . . . . b b b b . . . . . . 
@@ -17,12 +49,26 @@ function createFood () {
         4 d d d 4 4 4 . . . . . . . . . 
         4 4 4 4 . . . . . . . . . . . . 
         `, SpriteKind.Food)
+    tiles.placeOnRandomTile(pizza, sprites.castle.tileDarkGrass3)
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    music.baDing.play()
+    otherSprite.destroy()
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (canEatGhost == false) {
+        game.over(false)
+    } else {
+        otherSprite.destroy()
+    }
+})
+let star: Sprite = null
 let pizza: Sprite = null
-let Zacman = sprites.create(img`
+let canEatGhost = false
+let Ghost: Sprite = null
+let Zacman: Sprite = null
+Zacman = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -40,26 +86,41 @@ let Zacman = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(Zacman, 200, 200)
+controller.moveSprite(Zacman, 150, 150)
 tiles.setTilemap(tilemap`level1`)
-let Ghost = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . f . 1 1 1 1 f . . . . . . 
-    . . . 1 f 1 1 1 f 1 . . . . . . 
-    . . . 1 1 f 1 f 1 1 1 . . . . 1 
-    . . 1 1 1 1 1 1 1 1 1 1 . 1 1 1 
-    . . 1 1 f 1 1 1 f 1 1 1 1 1 1 . 
-    . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-    . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-    . . 1 f 1 1 1 1 1 f 1 1 1 1 . . 
-    . . 1 1 f f f f f 1 1 1 1 1 . . 
-    . . . 1 1 1 1 3 3 1 1 1 1 1 . . 
-    . . . . . 1 1 1 1 1 1 1 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
-Ghost.follow(Zacman, 50)
-scene.cameraFollowSprite(Zacman)
 tiles.placeOnRandomTile(Zacman, assets.tile`myTile`)
+for (let index = 0; index < 20; index++) {
+    createFood()
+}
+for (let index = 0; index < 4; index++) {
+    createGhost()
+}
+game.onUpdate(function () {
+    if (sprites.allOfKind(SpriteKind.Food).length == 0) {
+        game.over(true)
+    }
+})
+forever(function () {
+    music.playMelody("B A G A G F A C5 ", 120)
+})
+game.onUpdateInterval(20000, function () {
+    star = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 3 3 . . . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . 3 2 2 3 1 1 1 1 3 2 2 . . . 
+        . . 3 3 1 1 1 1 1 1 1 1 3 3 . . 
+        . . 3 3 1 1 1 1 1 1 1 1 3 3 . . 
+        . . . 3 1 1 1 1 1 1 1 1 3 . . . 
+        . . . . 3 1 1 1 1 1 1 3 . . . . 
+        . . . . 2 1 1 1 1 1 1 2 . . . . 
+        . . . . 2 1 1 3 3 1 1 2 . . . . 
+        . . . . 3 3 3 2 2 2 3 3 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.PowerUp)
+    tiles.placeOnRandomTile(star, sprites.castle.tileDarkGrass3)
+})
